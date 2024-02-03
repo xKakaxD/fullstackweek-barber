23032d33caedca62a32cc3383fe0cb193e7b02da
xKakaxD <ti.demoro@gmail.com>
2024-02-02 23:31:06 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
-import  Header from "../_components/header"
+import{format} from 'date-fns';
+import  Header from "../_components/ui/header";
+import { ptBR } from 'date-fns/locale';
 
 export default function Home (){
-    return <div>
-       <Header/>
-    </div>
+    return (
+        <div>
+            <Header/>
 
+            <div className='px-5 py-5 flex-row'>
+                <h2 className='text-xl font-bold'>Olá, Kaká</h2>
+                <p className = "capitalize text-sm">
+                    {format(new Date(), "EEEE,' 'dd' de 'LLLL", {
+                     locale: ptBR,
+                    })}
+                </p>
+            </div>
+        </div>
+
+    );
 }
--- app/_components/ui/header.tsx
+++ app/_components/ui/header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button";
+import { Card, CardContent } from "./card";
+import { Button } from "./button";
 import { MenuIcon } from "lucide-react";
 
 const Header = () => {
@@ -14,6 +14,6 @@ const Header = () => {
             </CardContent>
         </Card>
     );
-};
+}
  
 export default Header;
