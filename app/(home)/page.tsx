3d8b6a51481e4f0031d2b8f2b269b938f7bfd63a
xKakaxD <ti.demoro@gmail.com>
2024-02-02 22:29:08 -0300

    feat: initial header component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
+import  Header from "../_components/header"
 
 export default function Home (){
-    return <h1>teste</h1>;
+    return <div>
+       <Header/>
+    </div>
+
 }
